7de105df4bb688b53ba3fa74c05a7507446f75c4
Anush02198 <Anush.4376@gmail.com>
2022-07-08 00:59:08 +0530

    SpiceOS_Web:Initial Start
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ChangelogComponent } from './components/changelog/changelog.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch: 'full'
+},
+  {path:'home' ,component:HomeComponent},
+  {path:'downloads' ,component:DownloadsComponent},
+  {path:'about' ,component:AboutComponent},
+  {path:'features' ,component:FeaturesComponent},
+  {path:'changelog' ,component:ChangelogComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
+import { ChangelogComponent } from './components/changelog/changelog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    FeaturesComponent,
+    DownloadsComponent,
+    ChangelogComponent
   ],
   imports: [
     BrowserModule,
