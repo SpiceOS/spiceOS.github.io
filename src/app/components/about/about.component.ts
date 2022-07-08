fa49f79121e28ab16e7e027cbd6cbb21bd4c1320
Anush02198 <Anush.4376@gmail.com>
2022-07-09 00:54:43 +0530

    SpiceOS_Web:Initial code for our About Page
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as story from '../../../assets/json/story_time.json';
+import * as corepeps from '../../../assets/json/core_devs.json';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  fullStory:any;
+  core_Devs:any;
   constructor() { }
 
   ngOnInit(): void {
+    let story_temp = story;
+    let corepep = corepeps;
+    this.fullStory = story_temp.data;
+    this.core_Devs = corepep.data;
   }
 
 }
