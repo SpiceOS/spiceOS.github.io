b36ccdc0cfa07965acb8dce6cc94f5cbf14d3045
Anush02198 <Anush.4376@gmail.com>
2022-07-10 21:35:20 +0530

    SpiceOS_Web:Initial device specific Download page
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- src/app/components/device/device.component.ts
+++ src/app/components/device/device.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import * as dfd from '../../../assets/json/download_handler.json';
+import * as moment from 'moment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-device',
@@ -11,26 +13,41 @@ export class DeviceComponent implements OnInit {
 
   download:any;
   AllDeviceData:any;
-  constructor() { }
+  id:string
+  device:any;
+  constructor(private router:ActivatedRoute) { 
+    this.id=this.router.snapshot.params['id']
+  }
 
   ngOnInit(): void {
     let defed = dfd;
     this.download = defed.data;
     this.AllDeviceData = [];
-    this.getalldata();
-    console.log(this.AllDeviceData);    
+    this.getalldata();  
+    console.log(this.AllDeviceData);
+    this.filterDevice();
   }
 
   getalldata(){
+    this.AllDeviceData = [...this.download]
     this.download.forEach((e:any,index:number) => {
         axios.get(e.ota_json).then((res)=>{
-          // console.log(res.data.response[0]);
-          this.AllDeviceData.push(res.data.response[0]);
+          let splitter = res.data.response[0].filename.split("-")
+          res.data.response[0].build_date = splitter[2];
+          res.data.response[0].build_date = moment(res.data.response[0].build_date).format('MMMM DD YYYY');
+          res.data.response[0].previous_build = res.data.response[0].download.split("SpiceOS");
+          // console.log(res.data.response[0].download.split("SpiceOS"))
+          this.AllDeviceData[index].ota_json = res.data.response[0];
         })
-        axios.get(e.changelogs).then((res)=>{
-          Object.assign(this.AllDeviceData[index],{"changelogs":res.data})
+        axios.get(e.changelogs).then((res2)=>{
+          this.AllDeviceData[index].changelogs = res2.data;
         })
       });
+  }
+
+  filterDevice(){
+    this.device = this.AllDeviceData.filter((s: { device_codename: string | any[]; }) => s.device_codename.includes(this.id))
+    console.log(this.device);
   }
 
 }
