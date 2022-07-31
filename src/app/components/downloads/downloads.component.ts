b6168e19ec84428e83db1e608ed4511a71b2e640
Anush02198 <anush.4376@gmail.com>
2022-07-31 19:59:12 +0530

    SpiceOS_web:Ad support for build date and version on Downloads page
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- src/app/components/downloads/downloads.component.ts
+++ src/app/components/downloads/downloads.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import * as dfd from '../../../assets/json/download_handler.json';
+import * as moment from 'moment';
 import { Router } from '@angular/router';
 
 @Component({
@@ -28,6 +29,9 @@ export class DownloadsComponent implements OnInit {
     this.download.forEach((e:any,index:number) => {
         axios.get(e.ota_json).then((res)=>{
           this.AllDeviceData[index].ota_json = res.data.response[0];
+          let splitter = res.data.response[0].filename.split("-")
+          res.data.response[0].build_date = splitter[3];
+          res.data.response[0].build_date = moment(res.data.response[0].build_date).format('MMMM DD YYYY');
         })
       });
       this.FilteredDeviceData = this.AllDeviceData;
